54a7f93556015c34681ae4cb72d5d956ef93e12a
Aaron Chen <aachen@seek.com.au>
2025-05-23 17:35:20 +1000

    add ABPrinter Solution

--- src/main/java/concurrency/ABPrinter.java
+++ src/main/java/concurrency/ABPrinter.java
@@ -18,5 +18,36 @@ class ABPrinter {
     public void b(Runnable printB) throws InterruptedException {
         printB.run();
     }
+}
 
+class ABPrinterSolution {
+
+    private final int n;
+    private final AtomicInteger i;
+    private final AtomicInteger j;
+
+    private final Semaphore aSemaphore = new Semaphore(1);
+    private final Semaphore bSemaphore = new Semaphore(0);
+
+    public ABPrinterSolution(int n) {
+        this.n = n;
+        this.i = new AtomicInteger(0);
+        this.j = new AtomicInteger(0);
+    }
+
+    public void a(Runnable printA) throws InterruptedException {
+        while (i.getAndIncrement() < n) {
+            aSemaphore.acquire();
+            printA.run();
+            bSemaphore.release();
+        }
+    }
+
+    public void b(Runnable printB) throws InterruptedException {
+        while (j.getAndIncrement() < n) {
+            bSemaphore.acquire();
+            printB.run();
+            aSemaphore.release();
+        }
+    }
 }
